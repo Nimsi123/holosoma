ae40898b5660712b81aaafc33ba3f5b23619d20f
Nima Rahmanian <nimsi@berkeley.edu>
2025-12-12 19:12:06 +0000

    Small changes.

--- src/holosoma_retargeting/parallel_ttg_retargeting_smplh.sh
+++ src/holosoma_retargeting/parallel_ttg_retargeting_smplh.sh
@@ -12,11 +12,15 @@ source ~/.holosoma_deps/miniconda3/bin/activate hssim
 # python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
 #     --qpos_npz demo_results_parallel/g1/robot_only/tt4d_mixtape/87TgS5kIP-M_148000_148690_0_7_0_7_original.npz
 
-python data_conversion/parallel_convert_data_format_mj.py \
-    --input_dir demo_results_parallel/g1/robot_only/tt4d_mixtape \
-    --output_dir converted_res_parallel/robot_only/tt4d_mixtape \
-    --output_fps 30 \
-    --data_format tt4d \
-    --object_name ground \
-    --max_workers 8 \
-    --file_pattern "*.npz"
+python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
+    --qpos_npz /home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/tt4d_mixtape/-7lbDSIKUak_232930_233555_0_1_2_2_original.npz
+
+# we don't even need this script. we run this in beyondmimic
+# python data_conversion/parallel_convert_data_format_mj.py \
+#     --input_dir demo_results_parallel/g1/robot_only/tt4d_mixtape \
+#     --output_dir converted_res_parallel/robot_only/tt4d_mixtape \
+#     --output_fps 30 \
+#     --data_format tt4d \
+#     --object_name ground \
+#     --max_workers 8 \
+#     --file_pattern "*.npz"
