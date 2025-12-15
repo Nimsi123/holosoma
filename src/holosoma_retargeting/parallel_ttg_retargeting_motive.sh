e5dd4944e4770ed72ab39714a01019004d27a074
Nima Rahmanian <nimsi@berkeley.edu>
2025-12-15 20:29:35 +0000

    Small changes.

--- src/holosoma_retargeting/parallel_ttg_retargeting_motive.sh
+++ src/holosoma_retargeting/parallel_ttg_retargeting_motive.sh
@@ -6,6 +6,16 @@ source ~/.holosoma_deps/miniconda3/bin/activate hssim
 # python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
 #     --qpos_npz "/home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/motive_gameplay/Take 2025-09-29 05.07.06 PM_part003_15_original.npz"
 
-# Nice and pretty long! I think this is Milo. 
+# # Nice and pretty long! I think this is Milo. 
+# python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
+#     --qpos_npz "/home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/motive_gameplay/Take 2025-09-29 05.33.02 PM_part000_1_original.npz"
+
+
+
+# python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
+#     --qpos_npz "/home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/motive_gameplay/Take_2025-09-30_03.38.11_PM_part000_9_3_original.npz"
+
+
+# debug
 python viser_player.py --robot_urdf models/g1/g1_29dof.urdf \
-    --qpos_npz "/home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/motive_gameplay/Take 2025-09-29 05.33.02 PM_part000_1_original.npz"
+    --qpos_npz "/home/nima/holosoma/src/holosoma_retargeting/demo_results_parallel/g1/robot_only/motive_gameplay/Take_2025-09-30_03.38.11_PM_part000_9_4_original.npz"
